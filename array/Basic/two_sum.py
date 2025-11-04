c9c11b69d0397889857a46329eb8a5ba95b15fb7
Rakibul Islam <rakibulislamarif793@gmail.com>
2025-11-04 10:29:54 +0600

    find a target value this value is summation of two numbers and return there index

---
+++ array/Basic/two_sum.py
@@ -0,0 +1,15 @@
+
+nums = [3,2,4] 
+
+# nums = [3,3]
+target = 6
+
+mp = {}
+for i,val in enumerate(nums):
+    need = target - val
+    if need in mp:
+        print(mp[need],i)
+        break
+
+    mp[val]=i
+
