7e01c5021063631ebc25711e54ece2a559de7112
Rakibul Islam <rakibulislamarif793@gmail.com>
2025-11-07 15:20:55 +0600

    find all leader in the nums..

---
+++ array/medium/find_leader_list.py
@@ -0,0 +1,11 @@
+
+nums = [1, 2, 5, 3, 1, 2]
+leader_list = []
+max_from_right = float('-inf')
+
+for i in range(len(nums)):
+    if nums[i] > max_from_right:
+        max_from_right = nums[i]
+        leader_list.append(nums[i])
+
+print(leader_list[::-1])
