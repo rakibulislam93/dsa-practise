5ea8453a403a5ef1ce41dffcf562a5fbf82d4d3d
Rakibul Islam <rakibulislamarif793@gmail.com>
2025-11-07 15:25:37 +0600

    solved loop traversing

--- array/medium/find_leader_list.py
+++ array/medium/find_leader_list.py
@@ -3,7 +3,7 @@ nums = [1, 2, 5, 3, 1, 2]
 leader_list = []
 max_from_right = float('-inf')
 
-for i in range(len(nums)):
+for i in range(len(nums)-1,-1,-1):
     if nums[i] > max_from_right:
         max_from_right = nums[i]
         leader_list.append(nums[i])
