91d42fd2cc68c3b32200be83e5a6ee540006de55
Rakibul Islam <rakibulislamarif793@gmail.com>
2025-11-09 10:52:36 +0600

    solved same problem using optimize approach time-complexity O(n)

--- array/medium/max_subarray_sum.py
+++ array/medium/max_subarray_sum.py
@@ -1,10 +1,10 @@
 
-# nums = [2, 3, 5, -2, 7, -4] # output -- 15
-nums = [1, -2, 3] # output -- 3
+nums = [2, 3, 5, -2, 7, -4] # output -- 15
+# nums = [1, -2, 3] # output -- 3
 
 
-max_sum = float('-inf')
-n = len(nums)
+# max_sum = float('-inf')
+# n = len(nums)
 
 # for i in range(n):
 #     for j in range(n):
@@ -15,11 +15,18 @@ n = len(nums)
 #         max_sum = max(max_sum,current_sum)
 
 
-for i in range(n):
-    current_sum = 0
-    for j in range(i,n):
-        current_sum += nums[j]
-        max_sum = max(max_sum,current_sum)
+# for i in range(n):
+#     current_sum = 0
+#     for j in range(i,n):
+#         current_sum += nums[j]
+#         max_sum = max(max_sum,current_sum)
+
+max_sum = nums[0]
+curr_sum = nums[0]
+
+for i in range(1,len(nums)):
+    curr_sum = max(nums[i],curr_sum + nums[i])
+    max_sum = max(curr_sum,max_sum)
 
 
 print(max_sum)
