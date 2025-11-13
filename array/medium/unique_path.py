38af5af645aca45ed9140faa835e27ae38607890
Rakibul Islam <rakibulislamarif793@gmail.com>
2025-11-13 12:28:32 +0600

    solved this problem using combination formula

--- array/medium/unique_path.py
+++ array/medium/unique_path.py
@@ -1,7 +1,7 @@
 
 # find unique path for reach start to end.... time complexity O(2^(m+n)) very slow.
 m = 3
-n = 2
+n = 3
 i,j =0,0
 dp = [[-1 for _ in range(n)] for _ in range(m)]
 def findUniquePath(m,n,i,j):
@@ -17,3 +17,18 @@ def findUniquePath(m,n,i,j):
 
 print(findUniquePath(m,n,i,j))
 print(dp)
+
+# class Solution {
+# public:
+#     int uniquePaths(int m, int n) {
+#         int total_step = m + n -2;
+#         int down_step = m-1;
+
+#         long long int ans = 1;
+
+#         for(int i=1; i<=down_step; i++){
+#             ans = ans*( total_step - down_step + i) / i;
+#         }
+#         return ans;
+#     }
+# };
