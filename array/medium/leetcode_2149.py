4002de8cd0bb5f8898783db09732c5aada63378a
Rakibul Islam <rakibulislamarif793@gmail.com>
2025-11-14 10:32:49 +0600

    some try to optimize solutions

--- array/medium/leetcode_2149.py
+++ array/medium/leetcode_2149.py
@@ -1,20 +1,37 @@
 
 nums = [3,1,-2,-5,2,-4]
-pos_arr = []
-neg_arr = []
-result = []
+
+# Brute force solution .................
+# pos_arr = []
+# neg_arr = []
+# result = []
+# for val in nums:
+#     if val>0:
+#         pos_arr.append(val)
+#     else:
+#         neg_arr.append(val)
+
+# for i in range(len(pos_arr)):
+#     result.append(pos_arr[i])
+#     result.append(neg_arr[i])
+
+# print(pos_arr)
+# print(neg_arr)
+# print(result)
+
+# Optimize solution ..............
+
+result = [0]* len(nums)
+pos_indx = 0
+neg_indx = 1
+
 for val in nums:
     if val>0:
-        pos_arr.append(val)
+        result[pos_indx] = val
+        pos_indx += 2
     else:
-        neg_arr.append(val)
+        result[neg_indx] = val
+        neg_indx += 2
+    
 
-for i in range(len(pos_arr)):
-    result.append(pos_arr[i])
-    result.append(neg_arr[i])
-
-
-
-print(pos_arr)
-print(neg_arr)
 print(result)
