c3a02a8197d754e0409e78c5aadec0a68ac5ea84
Rakibul Islam <rakibulislamarif793@gmail.com>
2026-02-04 23:56:11 +0600

    solved leetcode 896 problem using python

--- array/Basic/leetcode_896.py
+++ array/Basic/leetcode_896.py
@@ -1,20 +1,36 @@
 
-class Solution {
-public:
-    bool isMonotonic(vector<int>& nums) {
-        bool decrease = true;
-        bool increase = true;
-        int n = nums.size();
+# class Solution {
+# public:
+#     bool isMonotonic(vector<int>& nums) {
+#         bool decrease = true;
+#         bool increase = true;
+#         int n = nums.size();
 
-        for(int i = 0; i < n - 1; i++){
-            if(nums[i] < nums[i+1]){
-                decrease = false;
-            }
-            if(nums[i] > nums[i+1]){
-                increase = false;
-            }
-        }
+#         for(int i = 0; i < n - 1; i++){
+#             if(nums[i] < nums[i+1]){
+#                 decrease = false;
+#             }
+#             if(nums[i] > nums[i+1]){
+#                 increase = false;
+#             }
+#         }
 
-        return decrease || increase;
-    }
-};
+#         return decrease || increase;
+#     }
+# };
+
+from typing import List
+
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        decrease = True
+        increase = True
+        n = len(nums)
+        
+        for i in range(n - 1):
+            if nums[i] < nums[i + 1]:
+                decrease = False
+            if nums[i] > nums[i + 1]:
+                increase = False
+        
+        return decrease or increase
